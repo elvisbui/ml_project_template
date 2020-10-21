aa803d0072cd62855b0057681aa7eaabf59aeda3
Elvis Bui <elvis.bui@outlook.com>
2020-10-21 08:46:53 -0700

    add regression metrics class

--- src/metrics.py
+++ src/metrics.py
@@ -1,4 +1,44 @@
 from sklearn import metrics
+import numpy as np
+
+
+class RegressionMetrics:
+    def __init__(self):
+        self.metrics = {
+            'mea': self._mae,
+            'mse': self._mse,
+            'rmse': self._rmse,
+            'msle': self._msle,
+            'r2': self._r2,
+        }
+
+    def __call__(self, metric, y_true, y_pred):
+        if metric not in self.metrics:
+            raise Exception('Metric not implemented')
+        else:
+            return self.metrics[metric](y_true=y_true, y_pred=y_pred)
+
+    @staticmethod
+    def _mae(y_true, y_pred):
+        return metrics.mean_absolute_error(y_true, y_pred)
+
+    @staticmethod
+    def _mse(y_true, y_pred):
+        return metrics.mean_squared_error(y_true, y_pred)
+
+    def _rmse(self, y_true, y_pred):
+        return np.sqrt(self._mse(y_true, y_pred))
+
+    @staticmethod
+    def _msle(y_true, y_pred):
+        return metrics.mean_squared_log_error(y_true, y_pred)
+
+    def _rmsle(self, y_true, y_pred):
+        return np.sqrt(self._msle(y_true, y_pred))
+
+    @staticmethod
+    def _r2(y_true, y_pred):
+        return metrics.r2_score(y_true, y_pred)
 
 
 class ClassificationMetrics:
